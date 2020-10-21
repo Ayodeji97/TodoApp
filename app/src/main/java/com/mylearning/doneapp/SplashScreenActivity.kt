6ad22beb7f79bff8d581e05bf2e31f693785b3f6
Daniel Ayodeji <danielayodeji97@gmail.com>
2020-10-22 00:57:47 +0100

    sign up sign in ui

---
+++ app/src/main/java/com/mylearning/doneapp/SplashScreenActivity.kt
@@ -0,0 +1,16 @@
+package com.mylearning.doneapp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        startActivity(Intent(this@SplashScreenActivity, MainActivity::class.java))
+        finish()
+    }
+}
